832eaccfddeb3dcca4f91db97898e38774fb53f2
Rintaro Koinuma <koinuma@lepra.jp>
2020-04-16 10:42:09 +0900

    1

---
+++ Assets/Script/GameCont.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCont : MonoBehaviour
+{
+    public Canvas startCanvas;
+    public Canvas ruleDousurukaModal;
+    public Canvas BattleCanvas;
+    public Canvas ResultCanvas;
+
+    int playNinzu;
+    int kaitoMojisu;
+    int kaitoJokenFlag;
+    int syoriJokenFlag;
+    string ngWord;
+
+    private void Start()
+    {
+        startCanvas.SetActive(false);
+        ruleDousurukaModal;
+        BattleCanvas;
+        ResultCanvas;
+}
+
+    public void HajimeruBtn() {
+        startCanvas.enabled = false;
+        ruleDousurukaModal
+    }
+}
